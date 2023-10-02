5efb8d4d8f85cabec3ac7c3d74dc1acaef9f273d
amar99587 <trissiti19@gmail.com>
2023-10-02 23:05:30 +0100

    add storage images

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" :src='image.url || "../assets/logo.png"'>
+    <input type="file" @change="handleImageUpload" accept="image/*" alt="enter the Image">
+    <HelloWorld :msg="text" />
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
+<script setup>
 import HelloWorld from '@/components/HelloWorld.vue'
+import { http } from "../plugins/axios";
+import { ref } from 'vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+const text = "Welcome to Your VueJs App";
+const image = ref({
+  file: "",
+  url: ""
+});
+
+const create = async () => {
+  
+  const formData = new FormData();
+  formData.append("image", image.value.file);
+  for (var value of formData.values()) {
+  console.log(value); // logs "value1" and "value2"
+}
+  try {
+    const res = await http.post("/storage/image", formData
+      , {
+        headers: {
+          'Content-Type': 'multipart/form-data', // Set the content type to multipart/form-data
+        }
+      }
+    );
+    console.log(res);
+    image.value.url = res.data;
+  } catch (err) {
+    console.log(err);
   }
 }
+
+const handleImageUpload = (event) => {
+  const file = event.target.files[0];
+  // image.value.display = URL.createObjectURL(file);
+  image.value.file = file;
+  create();
+};
 </script>
